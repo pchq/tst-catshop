27d2ba99bbb9552cba3fdebf5c04fa5419a22b75
pchq <9292358@gmail.com>
2019-01-30 15:01:25 +0800

    Updates

---
+++ precache-manifest.9d60fcf495ea4e9f55314bf81b38ed73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aac872479187440da02da0392cfec2d6",
+    "url": "/tst-catshop/static/media/kat.aac87247.png"
+  },
+  {
+    "revision": "36150b740a44c962867a33eb95e1a464",
+    "url": "/tst-catshop/static/media/Exo2.0-Thin.36150b74.woff2"
+  },
+  {
+    "revision": "1913eeb8e08b7ca9efe2e6a6ed6df229",
+    "url": "/tst-catshop/static/media/Exo2.0-Thin.1913eeb8.woff"
+  },
+  {
+    "revision": "0e7543d993d0a983821d",
+    "url": "/tst-catshop/static/js/runtime~main.0e7543d9.js"
+  },
+  {
+    "revision": "0cb68ecc0a34c4a298f7",
+    "url": "/tst-catshop/static/js/main.0cb68ecc.chunk.js"
+  },
+  {
+    "revision": "dffad4e782046226678e",
+    "url": "/tst-catshop/static/js/1.dffad4e7.chunk.js"
+  },
+  {
+    "revision": "0cb68ecc0a34c4a298f7",
+    "url": "/tst-catshop/static/css/main.643c44f5.chunk.css"
+  },
+  {
+    "revision": "fbee085f9c17ea92dc9eb16a6288a0d2",
+    "url": "/tst-catshop/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/tst-catshop/precache-manifest.9d60fcf495ea4e9f55314bf81b38ed73.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/tst-catshop/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
